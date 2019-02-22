2d67cf27fb81438b1c6c83104541df61289ad8f9
TheEEs <VisualBasic2013@hotmail.com>
2019-02-22 12:26:06 +0700

    Update home.js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,11 @@
 $(document).on('turbolinks:load',function () {
-    new TypeIt('.type-it', {
+    new TypeIt('#medium.type-it', {
+        strings: ["Hello!","We", "Are", "\"YONKERS\""],
+        speed: 89,
+        breakLines: false,
+        waitUntilVisible: true,
+    }).go();
+    new TypeIt('#small.type-it', {
         strings: ["Hello!","We", "Are", "\"YONKERS\""],
         speed: 89,
         breakLines: false,
